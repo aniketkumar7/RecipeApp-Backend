5bd904dc4adf4f43dccb3c25fec996850d7fc1a4
aniketkumar7 <aniketkumar639597@gmail.com>
2024-10-12 19:43:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://foodapp-iota-five.vercel.app/",
-  })
-);
+app.use(cors());
 app.use(express.static("public"));
 
 
