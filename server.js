7071c3b884f4ca7d511e4d6f08ddeff6d1f34d32
aniketkumar7 <aniketkumar639597@gmail.com>
2024-10-12 17:22:24 +0530

    Deploy the Backend

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const { connect } = require("mongoose");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
+
+app.use("/", require("./routes/user"));
+app.use("/recipe", require("./routes/recipe"));
+
+connect(process.env.CONNECTION_STRING)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
