4bceae1baf29346c55643e780bbb69cc12c82fda
aniketkumar7 <aniketkumar639597@gmail.com>
2025-03-09 16:22:52 +0530

    Add support for URL-encoded data in server middleware

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(cors());
-
+app.use(express.urlencoded({ extended: true }));
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 
