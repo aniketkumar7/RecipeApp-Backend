711de2389bf0ef2bd5522d7096785512b681b3f0
aniketkumar7 <aniketkumar639597@gmail.com>
2025-04-16 21:55:08 +0530

    Refactor image upload configuration for improved code consistency

--- controller/recipe.js
+++ controller/recipe.js
@@ -4,16 +4,17 @@ const multer = require("multer");
 
 // Backend image upload configuration
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: function(req, file, cb) {
     cb(null, 'public/images/');
   },
-  filename: function (req, file, cb) {
+  filename: function(req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, uniqueSuffix + '-' + file.originalname);
   }
 });
 
 const upload = multer({ storage: storage });
+
 
 // create getRecipes function to get recipes
 const getRecipes = async (req, res) => {
