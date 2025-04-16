243c8d6f78e2921218d1ed0868dff2b050c0d24b
aniketkumar7 <aniketkumar639597@gmail.com>
2025-04-16 19:02:36 +0530

    Refactor recipe routes for improved readability and consistency in middleware usage

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,23 +1,20 @@
+// Backend/routes/recipeRoutes.js
 const express = require("express");
-const { getRecipes, getRecipe, addRecipe, editRecipe, deleteRecipe, upload } = require("../controller/recipe");
+const {
+    getRecipes,
+    getRecipe,
+    addRecipe,
+    editRecipe,
+    deleteRecipe
+} = require("../controller/recipe");
+const { upload } = require("../config/cloudinary");
 const verifyToken = require("../middleware/auth");
 const router = express.Router();
 
-// create route for home page
-// It is used to get all the recipes
 router.get("/", getRecipes);
-
-// Route for getting recipes by id
 router.get("/:id", getRecipe);
-
-// Route for adding recipes
 router.post("/", upload.single("file"), verifyToken, addRecipe);
-
-// Route for updating recipes
-router.put("/:id", upload.single("file"), editRecipe);
-
-// Route for deleting recipes
-router.delete("/:id", deleteRecipe);
-
+router.put("/:id", upload.single("file"), verifyToken, editRecipe);
+router.delete("/:id", verifyToken, deleteRecipe);
 
 module.exports = router;
